e6e2b895fd7f2f33269725bdd2654bf4051f0c4e
nickaris1 <issarisnick@outlook.com>
2026-03-13 14:59:25 +0200

    :)

--- lua/theprimeagen/lazy/avante.lua
+++ lua/theprimeagen/lazy/avante.lua
@@ -1,40 +1,45 @@
 return {
-  "yetone/avante.nvim",
-  event = "VeryLazy",
-  version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
-  opts = {
-    -- add any opts here
-    -- for example
-    provider = "copilot",
-    auto_suggestions_provider = 'copilot',
-    behaviour = {
-      auto_suggestions = false, -- Experimental stage
-    },
-    suggestion = {
-      debounce = 100,
-      throttle = 100,
-    } ,
-  },
-  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-  build = "make",
-  dependencies = {
-    "zbirenbaum/copilot.lua",
-    "nvim-treesitter/nvim-treesitter",
-    "stevearc/dressing.nvim",
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    --- The below dependencies are optional,
-    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-    "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
-    "ibhagwan/fzf-lua",              -- for file_selector provider fzf
-    "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
-    {
-      -- Make sure to set this up properly if you have lazy=true
-      'MeanderingProgrammer/render-markdown.nvim',
-      opts = {
-        file_types = { "markdown", "Avante" },
-      },
-      ft = { "markdown", "Avante" },
-    },
-  },
+	"yetone/avante.nvim",
+	event = "VeryLazy",
+	version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+	opts = {
+		-- add any opts here
+		-- for example
+		provider = "copilot",
+		auto_suggestions_provider = "copilot",
+		behaviour = {
+			auto_suggestions = false, -- Experimental stage
+		},
+		suggestion = {
+			debounce = 100,
+			throttle = 100,
+		},
+		providers = {
+			copilot = {
+				model = "gpt-5-mini",
+			},
+		},
+	},
+	-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+	build = "make",
+	dependencies = {
+		"zbirenbaum/copilot.lua",
+		"nvim-treesitter/nvim-treesitter",
+		"stevearc/dressing.nvim",
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+		--- The below dependencies are optional,
+		"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+		"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+		"ibhagwan/fzf-lua", -- for file_selector provider fzf
+		"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+		{
+			-- Make sure to set this up properly if you have lazy=true
+			"MeanderingProgrammer/render-markdown.nvim",
+			opts = {
+				file_types = { "markdown", "Avante" },
+			},
+			ft = { "markdown", "Avante" },
+		},
+	},
 }
--- lua/theprimeagen/remap.lua
+++ lua/theprimeagen/remap.lua
@@ -4,7 +4,6 @@ vim.keymap.set("n", "<leader>pv", "<cmd>NvimTreeToggle<cr>")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
@@ -39,15 +38,10 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set(
-  "n",
-  "<leader>ee",
-  "oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
-)
+vim.keymap.set("n", "<leader>ee", "oif err != nil {<CR>}<Esc>Oreturn err<Esc>")
 
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
-vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
-
+vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>")
+vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>")
 
 vim.keymap.set("n", "<leader>|", "<cmd>vsplit<cr>")
 vim.keymap.set("n", "<leader>-", "<cmd>split<cr>")
@@ -61,3 +55,27 @@ vim.keymap.set("n", "<A-S-Left>", "<cmd>vertical resize -5<cr>")
 vim.keymap.set("n", "<A-S-Right>", "<cmd>vertical resize +5<cr>")
 vim.keymap.set("n", "<A-S-Up>", "<cmd>resize -5<cr>")
 vim.keymap.set("n", "<A-S-Down>", "<cmd>resize +5<cr>")
+
+vim.keymap.set("n", "<leader>lf", function()
+	local file_path = vim.fn.expand("%:p")
+	if file_path == "" then
+		vim.notify("No file path found for current buffer", vim.log.levels.WARN)
+		return
+	end
+
+	local file_dir = vim.fs.dirname(file_path)
+	local pkg = vim.fs.find("package.json", { path = file_dir, upward = true })[1]
+	local cwd = pkg and vim.fs.dirname(pkg) or vim.fn.getcwd()
+	local target = file_path
+	if vim.startswith(file_path, cwd .. "/") then
+		target = file_path:sub(#cwd + 2)
+	end
+
+	local result = vim.system({ "yarn", "eslint", "--fix", target }, { cwd = cwd, text = true }):wait()
+	if result.code == 0 then
+		vim.notify("eslint --fix completed", vim.log.levels.INFO)
+    vim.cmd("edit") -- Reload the current buffer to reflect changes
+	else
+		vim.notify(result.stderr ~= "" and result.stderr or "eslint --fix failed", vim.log.levels.ERROR)
+	end
+end, { desc = "[Lint] fix" })
